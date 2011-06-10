bbbb14b2c1bf45009bc90bfdf4aa8b6d1ffffcea
James Pearce <github@tripleodeon.com>
2011-06-10 16:26:42 -0700

    Basic app architecture

--- app/app.js
+++ app/app.js
@@ -6,36 +6,7 @@ cb = new Ext.Application({
             fullscreen: true,
             cardSwitchAnimation: 'slide',
 
-            items: [
-                {
-                    // the list card
-                    id: 'listCard',
-                    layout: 'fit',
-                    dockedItems: [{
-                        // main top toolbar
-                        dock : 'top',
-                        xtype: 'toolbar',
-                        title: 'Please wait' // will get added once loaded
-                    }],
-                    items: {
-                        // the list itself, gets bound to the store programmatically once it's loaded
-                        id: 'dataList',
-                        xtype: 'list',
-                        store: null,
-                        itemTpl: '{name}'
-                    }
-                }, {
-                    // the details card
-                    id: 'detailCard',
-                    xtype: 'panel',
-                    dockedItems: [{
-                        // also has a toolbar
-                        dock : 'top',
-                        xtype: 'toolbar',
-                        title: ''
-                    }]
-                }
-            ]
+            html: "Hello world!"
     
         });
     }
